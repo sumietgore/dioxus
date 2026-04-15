95b922219503358d93cd4c5159c404b9a6d56c56
Sumit Gore <sumeetgore96@gmail.com>
2026-04-15 12:29:11 +0200

    raspberry pi

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use wgpu::Limits;
+// use wgpu::Limits;
 
 mod app;
 
 fn main() {
-    let limits = wgpu::Limits {
-        max_storage_buffers_per_shader_stage: 8,
-        ..Limits::downlevel_defaults()
-    };
+    // let limits = wgpu::Limits {
+    //     max_storage_buffers_per_shader_stage: 8,
+    //     ..Limits::downlevel_defaults()
+    // };
 
-    dioxus_native::launch_cfg(app::app, vec![], vec![Box::new(limits)]);
+    dioxus_native::launch_cfg(app::app, vec![], vec![]);
     // dioxus_native::launch(app::app)
 }
